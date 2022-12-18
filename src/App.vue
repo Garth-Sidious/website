7e9ac62b05eb1b5363d82317e443bf74e7cda065
Luke Garside <lcg37@uclive.ac.nz>
2022-12-18 17:23:04 +1300

    Cleaned up header

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
 import { ref, computed } from 'vue'
 import Minesweeper from './components/Minesweeper.vue';
 import Screensaver from './components/Screensaver.vue';
-import TheHeader from './components/TheHeader.vue';
 import NotFound from './components/NotFound.vue';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 const routes = {
-  '/': Minesweeper,
-  '/screensaver': Screensaver
+  '/': { view: Minesweeper, text: 'Minesweeper' },
+  '/screensaver': { view: Screensaver, text: 'Screensaver' }
 }
 
 const currentPath = ref(window.location.hash)
@@ -17,15 +18,23 @@ window.addEventListener('hashchange', () => {
 })
 
 const currentView = computed(() => {
-  console.log('manic', currentPath.value)
-  return routes[currentPath.value.slice(1) || '/'] || NotFound
+  return routes[currentPath.value.slice(1) || '/']?.view || NotFound
+})
+
+const currentPage = computed(() => {
+  return currentPath.value.slice(1) || '/'
 })
 
 </script>
 
-
-
 <template>
-  <TheHeader />
+   <header class="d-flex justify-content-center py-3 text-bg-dark header">
+    <ul class="nav nav-pills">
+      <template v-for="(info, route) of routes">
+        <li v-if="currentPage === route" class="nav-item"><a :href="'#' + route" class="nav-link active" aria-current="page">{{ info.text }}</a></li>
+        <li v-else class="nav-item"><a :href="'#' + route" class="nav-link">{{ info.text }}</a></li>
+      </template>
+    </ul>
+  </header>
   <component :is="currentView" />
 </template>
