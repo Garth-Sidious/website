e4334e21c84b447a5c32498e3cf4408d10038782
Luke Garside <lcg37@uclive.ac.nz>
2023-03-09 11:41:41 +1300

    Chromatica 0.0.1, added board states and resizing

--- src/components/Chromatica.vue
+++ src/components/Chromatica.vue
@@ -1,43 +1,63 @@
 <script setup>
 import { onMounted } from 'vue';
 
-const boardSize = 750;
-const hexSize = boardSize / 18
-const hexGlue = hexSize * 0.95
+const colors = ["#eed", "#d2d", "#f22", "#dd2", "#2f2", "#2dd", "#22f"];
+let boardSize;
+let hexSize;
+let hexGlue;
 let ctx;
+
+const exampleBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 4, 4, 4, 4, 0, 0, 0, 3, 0, 0, 0, 0, 5, 0, 0, 0, 6, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 onMounted(() => {
   const chromatica = document.getElementById("chromatica");
   ctx = chromatica.getContext("2d");
+  setBoardSize();
+  drawChromatica(exampleBoard);
+})
+
+window.onresize = () => {
+  console.log('resize!')
+  setBoardSize();
+  drawChromatica(exampleBoard);
+}
+
+function setBoardSize() {
+  boardSize = Math.min(window.innerWidth, window.innerHeight - 104);
+  hexSize = boardSize / 18;
+  hexGlue = hexSize * 0.95;
+}
+function drawChromatica(gameState) {
+  console.log(gameState)
+  ctx.canvas.width  = boardSize;
+  ctx.canvas.height = boardSize;
   ctx.beginPath();
   ctx.rect(0, 0, boardSize, boardSize);
   ctx.fill();
 
-  // Draw board
-  drawBoard();
+  drawBoard(gameState);
+}
 
-})
-
-function drawBoard() {
+function drawBoard(gameState) {
   for (let i = -4; i <= 4; i += 1) {
     for (let j = -4; j <= 4; j += 1) { 
       let k = -i - j;
       if (Math.abs(i) + Math.abs(j) + Math.abs(k) < 8 || (Math.abs(i) + Math.abs(j) + Math.abs(k) === 8 && i && j && k)) {
-        let x = boardSize / 2 + (j - k) * hexGlue;
-        let y = boardSize / 2 + i * hexGlue * (1 + Math.SQRT1_2)
-        drawHex(hexSize, x, y, "#eed");
+        const x = boardSize / 2 + (j - k) * hexGlue * Math.sqrt((3 + 2 * Math.SQRT2) / 6);
+        const y = boardSize / 2 + i * hexGlue * (1 + Math.SQRT1_2)
+        const boardInfo = gameState[i + 4 + 9 * (j + 4)]
+        drawHex(hexSize, x, y, colors[boardInfo]);
       }
     }
   }
   drawHex(hexSize / 4, boardSize / 2, boardSize / 2, "#000");
   let bonusSize =  2 * (hexGlue / Math.cos(Math.PI / 6) - hexSize);
   let baseSize = hexGlue * 4 * (1 + Math.SQRT1_2) + hexSize + bonusSize;
-  drawHomeBase(hexSize, boardSize / 2 + baseSize * Math.cos(1 * Math.PI / 6), boardSize / 2 + baseSize * Math.sin(1 * Math.PI / 6), 10, "#d2d");
-  drawHomeBase(hexSize, boardSize / 2 + baseSize * Math.cos(3 * Math.PI / 6), boardSize / 2 + baseSize * Math.sin(3 * Math.PI / 6), 0, "#f22");
-  drawHomeBase(hexSize, boardSize / 2 + baseSize * Math.cos(5 * Math.PI / 6), boardSize / 2 + baseSize * Math.sin(5 * Math.PI / 6), 2, "#dd2");
-  drawHomeBase(hexSize, boardSize / 2 + baseSize * Math.cos(7 * Math.PI / 6), boardSize / 2 + baseSize * Math.sin(7 * Math.PI / 6), 4, "#2f2");
-  drawHomeBase(hexSize, boardSize / 2 + baseSize * Math.cos(9 * Math.PI / 6), boardSize / 2 + baseSize * Math.sin(9 * Math.PI / 6), 6, "#2dd");
-  drawHomeBase(hexSize, boardSize / 2 + baseSize * Math.cos(11 * Math.PI / 6), boardSize / 2 + baseSize * Math.sin(11 * Math.PI / 6), 8, "#22f");
+  for (let i = 0; i < 6; i++) {
+    let x = boardSize / 2 + baseSize * Math.cos((1 + 2 * i) * Math.PI / 6);
+    let y = boardSize / 2 + baseSize * Math.sin((1 + 2 * i) * Math.PI / 6);
+    drawHomeBase(hexSize, x, y, -2 + 2 * i, colors[i + 1]);
+  }
 }
 
 function drawHex(size, x, y, color) {
